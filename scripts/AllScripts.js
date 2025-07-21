498bfb379546649b35488aec6780e3aad82c3e3f
jimbobewenhallMain <jimbobewenhall@gmail.com>
2025-07-21 13:45:13 +0100

    Script cleanup

--- scripts/AllScripts.js
+++ scripts/AllScripts.js
@@ -1,13 +1,19 @@
 let carouselInterval;
 let resizeTimer;
 let overlay;
+let car_top
+let car_bottom
+let car_left
+let car_right
+let car_bottom_text
+let zoom_images
+let fav_house
+let isZoomed
+let isAnimating
+let allParentSections
+let body
 
-// Definitions for image zooming on features page
-let isZoomed = false;
-let isAnimating = false; // Prevents conflicts from rapid-fire clicks.
-let allParentSections = document.querySelectorAll("section")
-
-const body = document.body;
+const optional_components = [['.carousel', 'M_Carousel'], ['.parallax', 'M_Parallax'], ['.collapsible', 'M_Collapsible']]
 
 gsap.registerPlugin(Flip);
 gsap.registerPlugin(ScrollToPlugin);
@@ -33,14 +39,14 @@ const swup = new Swup({
                 let tl = gsap.timeline();
 
                 let urlSearchParams = new URLSearchParams(window.location.search);
-                let anchor = urlSearchParams.get("anchor");
+                let anchor = urlSearchParams.get('anchor');
 
                 if (anchor) {
                     await tl.fromTo('#swup', {opacity: 0}, {opacity: 1, duration: 0.25})
                         .to(window, {
                             duration: 0.5,
                             scrollTo:{y:anchor, autoKill: true},
-                            ease: "power3.inOut"
+                            ease: 'power3.inOut'
                         })
                 } else {
                     await tl.fromTo('#swup', {opacity: 0}, {opacity: 1, duration: 0.25});
@@ -57,34 +63,17 @@ document.addEventListener('DOMContentLoaded', () => {pageInit()});
 window.addEventListener('load', () => {
     pageLoad();
 
+    let resizeEvent = () => {
+        pageInit();
+        pageLoad();
+
+        if (car_bottom_text) {debouncedAdjustText('.carousel-bottom-text')}
+        if (fav_house) {debouncedAdjustText('.fave-house', true)}
+    }
+
     // On window resize, resize items and adjust text
-    window.addEventListener("resize", () => {
-        pageInit();
-        pageLoad();
-
-        if (document.querySelector('.carousel-bottom-text')) {
-            debouncedAdjustText('.carousel-bottom-text')
-        }
-
-        if (document.querySelector('.fave-house')) {
-            debouncedAdjustText('.fave-house', true)
-        }
-
-    })
-
-    screen.orientation.addEventListener("change", () => {
-        pageInit();
-        pageLoad();
-
-        if (document.querySelector('.carousel-bottom-text')) {
-            debouncedAdjustText('.carousel-bottom-text')
-        }
-
-        if (document.querySelector('.fave-house')) {
-            debouncedAdjustText('.fave-house', true)
-        }
-
-    })
+    window.addEventListener('resize', () => {resizeEvent()})
+    screen.orientation.addEventListener('change', () => {resizeEvent()})
 });
 
 document.addEventListener('swup:page:view', () => {
@@ -101,56 +90,42 @@ const pageUnload = function () {
     M.Sidenav.getInstance(document.querySelector('.sidenav')).destroy();
 
     // Optional materialize components
-    if (document.querySelectorAll('.carousel')) {
-        let elems = document.querySelectorAll('.carousel')
-        elems.forEach(elem => {M.Carousel.getInstance(elem).destroy();})
-    }
-
-    if (document.querySelectorAll('.parallax')) {
-        let elems = document.querySelectorAll('.parallax')
-        elems.forEach(elem => {M.Parallax.getInstance(elem).destroy();})
-    }
-
-    if (document.querySelectorAll('.collapsible')) {
-        let elems = document.querySelectorAll('.collapsible')
-        elems.forEach(elem => {M.Collapsible.getInstance(elem).destroy();})
-    }
+    optional_components.forEach(component => {
+        let elems = document.querySelectorAll(component[0])
+        if (elems) {elems.forEach(elem => {elem[component[1]].destroy()})}
+    })
 
     clearInterval(carouselInterval)
 }
 
 const pageInit = function () {
+    // Update all the init variables (needed because of swup reload process)
+    isZoomed = false;
+    isAnimating = false;
+    allParentSections = document.querySelectorAll('section')
+
+    body = document.body;
+    car_top = document.querySelector('.carousel-top')
+    car_bottom = document.querySelector('.carousel-bottom')
+    car_left = document.querySelector('.carousel-left-bottom')
+    car_right = document.querySelector('.carousel-right-bottom')
+    car_bottom_text = document.querySelector('.carousel-bottom-text')
+    zoom_images = document.querySelectorAll('.zoom-image')
+    fav_house = document.querySelector('.fave-house')
+
+
     // Initialise Materialize components
+    M.AutoInit();
 
     // Sidenav on all pages
-    M.Sidenav.init(document.querySelectorAll('.sidenav'), {
-        edge: 'right'
-    });
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'right'});
 
-    // Optional materialize components
-    if (document.querySelectorAll('.carousel')) {
-        M.Carousel.init(document.querySelectorAll('.carousel'), {})
-    }
-
-    if (document.querySelector('.carousel-top')) {
-        M.Carousel.init(document.querySelectorAll('.carousel-top'), {
-            onCycleTo: nextSlide
-        });
-    }
-
-    if (document.querySelectorAll('.parallax')) {
-        M.Parallax.init(document.querySelectorAll('.parallax'), {});
-    }
-
-    if (document.querySelectorAll('.collapsible')) {
-        M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
-    }
+    if (car_top) {M.Carousel.init(car_top, {onCycleTo: nextSlide})}
 
     // Initialise video synchronisation
-    if (document.querySelector('.video-background') && document.querySelector('.video-foreground')) {
-        const backgroundVideo = document.querySelector('.video-background');
-        const foregroundVideo = document.querySelector('.video-foreground');
-
+    const backgroundVideo = document.querySelector('.video-background');
+    const foregroundVideo = document.querySelector('.video-foreground');
+    if (backgroundVideo && foregroundVideo) {
         // Function to synchronise the videos
         const syncVideos = () => {
             if (backgroundVideo.readyState >= 3 && foregroundVideo.readyState >= 3) {
@@ -174,30 +149,21 @@ const pageInit = function () {
         })
     }
 
-    if (document.querySelector('.carousel-left-bottom') && document.querySelector('.carousel-right-bottom')) {
-        // On carousel bottom click, move carousel to next or previous item
-        document.querySelector('.carousel-left-bottom').addEventListener('click', () => {
-            moveCarousel(document.querySelector('.carousel-bottom'), 'prev')
-        })
-
-        document.querySelector('.carousel-right-bottom').addEventListener('click', () => {
-            moveCarousel(document.querySelector('.carousel-bottom'), 'next')
-        })
+    if (car_left && car_right) {
+        car_left.addEventListener('click', () => {moveCarousel(car_bottom, 'prev')})
+        car_right.addEventListener('click', () => {moveCarousel(car_bottom, 'next')})
     }
 
-    if (document.querySelector('.zoom-image')) {
-        const images = document.querySelectorAll(".zoom-image");
+    if (zoom_images) {
+        overlay = document.createElement('div');
+        overlay.classList.add('zoom-overlay');
 
-        overlay = document.createElement("div");
-        overlay.classList.add("zoom-overlay");
-
-        images.forEach(image => {
-            image.addEventListener("click", zoomIn);
+        zoom_images.forEach(image => {
+            image.addEventListener('click', zoomIn);
         })
 
-        overlay.addEventListener("click", zoomOut);
+        overlay.addEventListener('click', zoomOut);
     }
-
 }
 
 const pageLoad = function () {
@@ -206,28 +172,19 @@ const pageLoad = function () {
     // Resize items and adjust text
     resizeItems()
 
-    if (document.querySelector('.carousel-bottom-text')) {
-        FitTextToBox('.carousel-bottom-text')
-    }
+    if (car_bottom_text) {FitTextToBox('.carousel-bottom-text')}
+    if (fav_house) {FitTextToBox('.fave-house', true)}
 
-    if (document.querySelector('.fave-house')) {
-        FitTextToBox('.fave-house', true)
-    }
-
-    if (document.querySelector('.carousel-top')) {
+    if (car_top) {
         clearInterval(carouselInterval)
-        carouselInterval = setInterval(moveCarousel, 5000,
-            document.querySelector('.carousel-top'), 'next');
+        carouselInterval = setInterval(moveCarousel, 5000, car_top, 'next');
     }
 
-    if (document.querySelector('.zoom-image')) {
-        let allParentSections = document.querySelectorAll("section")
+    if (zoom_images) {
         allParentSections.forEach(section => {
-            if (section.querySelector(".zoom-image")) {
-                let subSection = section.querySelector(".container");
-                let currentHeight = subSection.clientHeight;
-
-                subSection.style.height = currentHeight + "px";
+            if (section.querySelector('.zoom-image')) {
+                let subSection = section.querySelector('.container');
+                subSection.style.height = subSection.clientHeight + 'px';
             }
         })
     }
@@ -241,30 +198,30 @@ function zoomIn() {
     let parentSectionID = image.parentNode.parentNode.parentNode.parentNode.id
 
     allParentSections.forEach(section => {
-        if (section.querySelector(".zoom-image") && section.id !== parentSectionID) {
+        if (section.querySelector('.zoom-image') && section.id !== parentSectionID) {
               section.style.zIndex = -1;
         }
     })
 
-    if (isZoomed || isAnimating) return;
+    if (isZoomed || isAnimating) {return}
     isAnimating = true;
 
     const state = Flip.getState(image);
 
     body.appendChild(overlay);
-    image.classList.add("image-zoomed");
 
-    image.scrollIntoView({behavior: "smooth"})
+    image.classList.add('image-zoomed');
+    image.scrollIntoView({behavior: 'smooth'})
 
-    body.classList.add("body-no-scroll");
+    body.classList.add('body-no-scroll');
 
     gsap.to(overlay, {opacity: 1, duration: 0.6});
 
     Flip.from(state, {
         duration: 0.6,
-        ease: "power3.inOut",
-        scale: true,      // Animate scale for performance.
-        absolute: true,   // Isolate the element from layout shifts.
+        ease: 'power3.inOut',
+        scale: true,
+        absolute: true,
         onComplete: () => {
             isZoomed = true;
             isAnimating = false;
@@ -282,23 +239,20 @@ function zoomOut() {
 
     const state = Flip.getState(image);
 
-    image.classList.remove("image-zoomed");
-    body.classList.remove("body-no-scroll");
+    image.classList.remove('image-zoomed');
+    body.classList.remove('body-no-scroll');
 
     gsap.to(overlay, {
         opacity: 0,
         duration: 0.6,
         onComplete: () => {
-            // Only remove the overlay from the DOM after it's invisible.
-            if (overlay.parentNode) {
-                body.removeChild(overlay);
-            }
+            if (overlay.parentNode) {body.removeChild(overlay)}
         }
     });
 
     Flip.from(state, {
         duration: 0.6,
-        ease: "power3.inOut",
+        ease: 'power3.inOut',
         scale: true,
         absolute: true,
         onComplete: () => {
@@ -306,7 +260,7 @@ function zoomOut() {
             isAnimating = false;
 
             allParentSections.forEach(section => {
-                if (section.querySelector(".zoom-image")) {
+                if (section.querySelector('.zoom-image')) {
                     section.style.zIndex = 1;
                 }
             })
@@ -324,18 +278,18 @@ const nextSlide = function (activeSlide) {
     gsap.set(h2, {opacity: 1});
 
     let split = SplitText.create(h2, {
-        type: "words",
-        wordsClass: "word++",
-        wordDelimiter: " "
+        type: 'words',
+        wordsClass: 'word++',
+        wordDelimiter: ' '
     });
 
     gsap.from(split.words, {
         y: -50,
         opacity: 0,
-        rotation: "random(-80, 80)",
+        rotation: 'random(-80, 80)',
         stagger: 0.2,
         duration: 1,
-        ease: "back"
+        ease: 'back'
     });
 
     let tl = gsap.timeline({repeat: 1});
@@ -343,15 +297,16 @@ const nextSlide = function (activeSlide) {
     tl.to(icon, {
         scale: 1.3,
         duration: 0.3,
-        ease: "back"
+        ease: 'back'
     })
         .to(icon, {
             scale: 1,
             duration: 0.3,
-            ease: "back"
+            ease: 'back'
         })
 }
 
+// Generic transform function for all browsers
 const transformItem = function (elem, transform) {
     elem.style.transform = transform
     elem.style.webkitTransform = transform
@@ -363,17 +318,16 @@ const transformItem = function (elem, transform) {
 function resizeItems() {
     // Function to resize webpage items on the window load and window resize
 
-    if (document.querySelector('.height-parent')) {
-        let parents = document.querySelectorAll('.height-parent')
+    let parents = document.querySelectorAll('.height-parent')
+    if (parents) {
         parents.forEach(parent => {
-            const definer = parent.querySelector('.height-definer');
-            const child  = parent.querySelector('.height-child');
+            let definer = parent.querySelector('.height-definer');
+            let child  = parent.querySelector('.height-child');
 
-            const H = definer.clientHeight;
+            let H = definer.clientHeight;
             parent.style.height = H + 'px';
 
-            const hChild = child.clientHeight;
-            const offset = (H - hChild) / 2;
+            let offset = (H - child.clientHeight) / 2;
 
             child.style.paddingTop  = offset + 'px';
         })
@@ -385,60 +339,60 @@ function resizeItems() {
         let phone_height = phone_width * 1.97073170732
 
         let video_element = document.querySelector('.phone-video')
-        let translation = "translate("+ (phone_width * 0.073) +"px, -" + (phone_height * 0.98) + "px)"
+        let translation = 'translate(' + (phone_width * 0.073) + 'px, -' + (phone_height * 0.98) + 'px)'
 
-        video_element.style.width = (phone_width * 0.87) + "px"
+        video_element.style.width = (phone_width * 0.87) + 'px'
         transformItem(video_element, translation)
 
-        document.querySelector('.video-foreground').style.height = (phone_height * 0.955) + "px"
+        document.querySelector('.video-foreground').style.height = (phone_height * 0.955) + 'px'
 
         // Reformat background gradient to match the height of the phone SVG container
-        document.querySelector('.gradient-container').style = "background: linear-gradient(transparent " + phone_height * 0.28 + "px, #ffa000 " + phone_height * 0.30 + "px, #ff6a00 100%);" +
-            "background: -moz-linear-gradient(transparent " + phone_height * 0.28 + "px, #ffa000 " + phone_height * 0.30 + "px, #ff6a00 100%); " +
-            "background: -webkit-gradient(linear, left top, left bottom, color-stop(" + phone_height * 0.28 + "px,transparent), color-stop(" + phone_height * 0.30 + "px,#ffa000), color-stop(100%,#ff6a00)); " +
-            "background: -webkit-linear-gradient(transparent " + phone_height * 0.28 + "px, #ffa000 " + phone_height * 0.30 + "px, #ff6a00 100%); " +
-            "background: -o-linear-gradient(transparent " + phone_height * 0.28 + "px, #ffa000 " + phone_height * 0.30 + "px, #ff6a00 100%); " +
-            "background: -ms-linear-gradient(transparent " + phone_height * 0.28 + "px, #ffa000 " + phone_height * 0.30 + "px, #ff6a00 100%); "
+        document.querySelector('.gradient-container').style = 'background: linear-gradient(transparent ' + phone_height * 0.28 + 'px, #ffa000 ' + phone_height * 0.30 + 'px, #ff6a00 100%);' +
+            'background: -moz-linear-gradient(transparent ' + phone_height * 0.28 + 'px, #ffa000 ' + phone_height * 0.30 + 'px, #ff6a00 100%); ' +
+            'background: -webkit-gradient(linear, left top, left bottom, color-stop(' + phone_height * 0.28 + 'px,transparent), color-stop(' + phone_height * 0.30 + 'px,#ffa000), color-stop(100%,#ff6a00)); ' +
+            'background: -webkit-linear-gradient(transparent ' + phone_height * 0.28 + 'px, #ffa000 ' + phone_height * 0.30 + 'px, #ff6a00 100%); ' +
+            'background: -o-linear-gradient(transparent ' + phone_height * 0.28 + 'px, #ffa000 ' + phone_height * 0.30 + 'px, #ff6a00 100%); ' +
+            'background: -ms-linear-gradient(transparent ' + phone_height * 0.28 + 'px, #ffa000 ' + phone_height * 0.30 + 'px, #ff6a00 100%); '
 
         // Resize the bottom carousel to fit image and title inside
-        document.querySelector('.carousel-bottom').style.height = (document.querySelector('.carousel-item-bottom').clientWidth * 1.25) + "px"
-        document.querySelector('.carousel-bottom-text').style.height = (document.querySelector('.carousel-item-bottom').clientWidth * 0.25) + "px"
+        let car_item_bottom = document.querySelector('.carousel-item-bottom')
+        car_bottom.style.height = (car_item_bottom.clientWidth * 1.25) + 'px'
+        car_bottom_text.style.height = (car_item_bottom.clientWidth * 0.25) + 'px'
 
         // Move left and right buttons for carousel bottom to be halfway down the carousel
         let car_height = document.querySelector('.carousel-row').clientHeight
-        let button_height = document.querySelector('.carousel-left-bottom').clientHeight
+        let button_height = car_left.clientHeight
         car_height = (car_height * 0.5) - (button_height * 0.5)
 
-        transformItem(document.querySelector('.carousel-left-bottom'), "translateY(" + car_height + "px)")
-        transformItem(document.querySelector('.carousel-right-bottom'), "translateY(" + car_height + "px)")
+        transformItem(car_left, 'translateY(' + car_height + 'px)')
+        transformItem(car_right, 'translateY(' + car_height + 'px)')
     }
 
-    if (document.querySelector('#people')) {
-        let people = document.querySelector('#people')
+    let people = document.querySelector('#people')
+    if (people) {
         let person = people.querySelectorAll('.person')
 
         person.forEach(elem => {
             let image_row = elem.querySelector('.image-row')
             let quote_row = elem.querySelector('.quote-row')
 
-            quote_row.style.height = image_row.clientHeight + "px"
-            }
-        )
+            quote_row.style.height = image_row.clientHeight + 'px'
+        })
     }
 
-    if (document.querySelectorAll('.quote-text')) {
-        let elems = document.querySelectorAll('.quote-text')
-        elems.forEach(elem => {
+    let quote_elems = document.querySelectorAll('.quote-text')
+    if (quote_elems) {
+        quote_elems.forEach(elem => {
             let quote_text = elem.querySelector('.quote-text-inner')
             let quote_left = elem.querySelector('.quote-left')
             let quote_right = elem.querySelector('.quote-right')
 
             let quote_bbox = quote_text.getBoundingClientRect()
 
-            quote_left.style.left = (quote_bbox.left - quote_left.getBoundingClientRect().width) + "px"
-            quote_left.style.top = ((quote_bbox.top - quote_bbox.height) - quote_left.getBoundingClientRect().height * 2) + "px"
-            quote_right.style.right = (quote_bbox.left - quote_left.getBoundingClientRect().width) + "px"
-            quote_right.style.top = (quote_bbox.top - quote_right.getBoundingClientRect().height) + "px"
+            quote_left.style.left = (quote_bbox.left - quote_left.getBoundingClientRect().width) + 'px'
+            quote_left.style.top = ((quote_bbox.top - quote_bbox.height) - quote_left.getBoundingClientRect().height * 2) + 'px'
+            quote_right.style.right = (quote_bbox.left - quote_left.getBoundingClientRect().width) + 'px'
+            quote_right.style.top = (quote_bbox.top - quote_right.getBoundingClientRect().height) + 'px'
         })
     }
 }
